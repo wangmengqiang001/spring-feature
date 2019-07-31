c3ea28c7dcd08e5dab4b54a6b0c00cec1d5d5927
wangmengqiang001 <wangmengqiang.hr@gmail.com>
2019-07-31 16:10:36 +0800

    close issue #[3,5] by [wangmengqiang]

--- src/main/java/osgi/web/controller/HelloWorld.java
+++ src/main/java/osgi/web/controller/HelloWorld.java
@@ -6,7 +6,16 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
+import osgi.web.beans.UserInfo;
+
 public class HelloWorld implements Controller {
+	private UserInfo user;
+	
+
+	public void setUser(UserInfo user) {
+		this.user = user;
+	}
+
 
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		  String userName = request.getParameter("user");
@@ -14,6 +23,8 @@ public class HelloWorld implements Controller {
 	      if (userName != null)
 	      {
 	        result = "Hello, " + userName + "!";
+	      }else {
+	    	  result = "Hello," + user;
 	      }
 
 	      ModelAndView view = new ModelAndView("hello_view");
--- src/main/java/osgi/web/servlet/HelloServlet.java
+++ src/main/java/osgi/web/servlet/HelloServlet.java
@@ -6,13 +6,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+
+import osgi.web.beans.UserInfo;
+
 /**
  * Servlet implementation class HelloServlet
  */
+@Configurable
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	@Autowired
+	private UserInfo userinfo;
        
-    /**
+    public void setUser(UserInfo user) {
+		this.userinfo = user;
+	}
+
+	/**
      * @see HttpServlet#HttpServlet()
      */
     public HelloServlet() {
@@ -25,7 +38,10 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Hello, Served at: ").append(request.getContextPath());
+		//userinfo = new UserInfo();
+		//userinfo.setAccount("accountInfo");
+		//userinfo.setPassword("passwordAdmin");
+		response.getWriter().append("Hello, Served at: 21 " + userinfo).append(request.getContextPath());
 	}
 
 	/**
